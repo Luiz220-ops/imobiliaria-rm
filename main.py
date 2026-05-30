f0c124b4a603b3b1e14ed532470d98bd2def91a3
Luiz220-ops <luizotaviops.14@gmail.com>
2026-05-30 14:35:33 -0300

    feat: implementa a subclasse Apartamento com regra de desconto

--- main.py
+++ main.py
@@ -8,3 +8,24 @@ class Imovel:
 
     def calcular_aluguel(self):
         return self.valor_base
+        
+class Apartamento(Imovel):
+    """Subclasse para gerenciar as regras específicas de Apartamentos."""
+    def __init__(self, quartos, tem_garagem, tem_criancas):
+        super().__init__("Apartamento", 700.00)  # Valor base para 1 quarto
+        self.quartos = quartos
+        self.tem_garagem = tem_garagem
+        self.tem_criancas = tem_criancas
+
+    def calcular_aluguel(self):
+        valor = self.valor_base
+        # Adicional por quarto extra
+        if self.quartos == 2:
+            valor += 200.00
+        # Adicional de vaga de garagem
+        if self.tem_garagem:
+            valor += 300.00
+        # Desconto de 5% se não houver crianças
+        if not self.tem_criancas:
+            valor *= 0.95
+        return valor
